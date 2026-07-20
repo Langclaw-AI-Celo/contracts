ada4e18ab63a68cf6f75436f2aed492dec262a53
Nant361 <ridhorinantaa45@gmail.com>
2026-07-20 14:19:35 +0700

    test(vault): extract shared fixtures

--- test/LangclawUsageVault.t.sol
+++ test/LangclawUsageVault.t.sol
@@ -4,10 +4,16 @@ pragma solidity ^0.8.20;
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {Pausable} from "@openzeppelin/contracts/utils/Pausable.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Test} from "forge-std/Test.sol";
 import {LangclawUsageVault} from "../src/LangclawUsageVault.sol";
+import {
+    FailingTransferFromToken,
+    FailingTransferToken,
+    MockUSDT,
+    ReentrantWithdrawalReceiver,
+    RevertingWithdrawalReceiver
+} from "./helpers/LangclawUsageVaultFixtures.sol";
 
 contract LangclawUsageVaultTest is Test {
     LangclawUsageVault internal vault;
@@ -954,52 +960,6 @@ contract LangclawUsageVaultTest is Test {
     }
 }
 
-contract ReentrantWithdrawalReceiver {
-    LangclawUsageVault internal immutable vault;
-
-    bool public reentryBlocked;
-    bytes4 public lastRevertSelector;
-
-    constructor(LangclawUsageVault vault_) {
-        vault = vault_;
-    }
-
-    receive() external payable {
-        try vault.withdraw(1 wei) {
-            reentryBlocked = false;
-        } catch (bytes memory reason) {
-            reentryBlocked = true;
-            if (reason.length >= 4) {
-                bytes4 selector;
-                assembly {
-                    selector := mload(add(reason, 0x20))
-                }
-                lastRevertSelector = selector;
-            }
-        }
-    }
-
-    function attack(uint256 amount) external {
-        vault.withdraw(amount);
-    }
-}
-
-contract RevertingWithdrawalReceiver {
-    LangclawUsageVault internal immutable vault;
-
-    constructor(LangclawUsageVault vault_) {
-        vault = vault_;
-    }
-
-    receive() external payable {
-        revert("native-transfer-rejected");
-    }
-
-    function withdrawFromVault(uint256 amount) external {
-        vault.withdraw(amount);
-    }
-}
-
 contract LangclawUsageVaultTokenTest is Test {
     LangclawUsageVault internal vault;
     MockUSDT internal usdt;
@@ -1374,52 +1334,6 @@ contract LangclawUsageVaultTokenTest is Test {
         assertEq(vault.authorizedWithdrawals(payer), 0);
         assertEq(vault.totalAuthorizedWithdrawals(), 0);
         assertEq(vault.totalWithdrawn(), withdrawalAmount);
-    }
-}
-
-contract MockUSDT is ERC20 {
-    constructor() ERC20("Mock USDT", "USDT") {}
-
-    function decimals() public pure override returns (uint8) {
-        return 6;
-    }
-
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount);
-    }
-}
-
-contract FailingTransferToken is ERC20 {
-    bool internal transferFails;
-
-    constructor() ERC20("Failing Token", "FAIL") {}
-
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount);
-    }
-
-    function setTransferFailure(bool shouldFail) external {
-        transferFails = shouldFail;
-    }
-
-    function transfer(address to, uint256 amount) public override returns (bool) {
-        if (transferFails) {
-            return false;
-        }
-
-        return super.transfer(to, amount);
-    }
-}
-
-contract FailingTransferFromToken is ERC20 {
-    constructor() ERC20("Failing TransferFrom Token", "FAIL_FROM") {}
-
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount);
-    }
-
-    function transferFrom(address, address, uint256) public pure override returns (bool) {
-        return false;
     }
 }
 
